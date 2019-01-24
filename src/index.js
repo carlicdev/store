66e800840a754e71a17602f26280c855336b9702
carlicdev <carlic.dev@gmail.com>
2019-01-24 15:28:36 -0600

    commiting

--- src/index.js
+++ src/index.js
@@ -20,4 +20,29 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+/*# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+*/
+
 serviceWorker.unregister();
